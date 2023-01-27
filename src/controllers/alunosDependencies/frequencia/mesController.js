e9df940a9224c65ffac3f6d367a680e58d8d9e72
david nascimento <viniciosdavid1@gmail.com>
2023-01-27 08:46:25 -0300

    alterando updatemes

--- src/controllers/alunosDependencies/frequencia/mesController.js
+++ src/controllers/alunosDependencies/frequencia/mesController.js
@@ -29,6 +29,9 @@ exports.getMesByFrequenciaId = async (req, res) => {
         let response = [];
         for (let i = 0; i < meses.length; i++) {
             const aux = [
+                {
+                    id: meses[i].id
+                },
                 {   
                     id: 01,
                     dia: meses[i].a01
@@ -178,13 +181,12 @@ exports.createMes = async (req, res) => {
 exports.putMes = async (req, res) => {
     try {
         const { id } = req.params;
-        const {  frequenciaId, a01, a02, a03, a04, a05, a06, a07, a08, a09, a10, a11, a12, a13, a14, a15, a16, a17, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, nome, } = req.body;
+        const { a01, a02, a03, a04, a05, a06, a07, a08, a09, a10, a11, a12, a13, a14, a15, a16, a17, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31,  } = req.body;
         let mes = await prisma.mes.findUnique({ where: { id: Number(id) } });
         if (!mes) { return res.status(404).json("mes não cadastrado") }
         mes = await prisma.mes.update({
             where: { id: Number(id) },
-            data: { frequenciaId, a01, a02, a03, a04, a05, a06, a07, a08, a09, a10, a11, a12, a13, a14, a15, a16, a17, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31, nome,
-            }
+            data: { a01, a02, a03, a04, a05, a06, a07, a08, a09, a10, a11, a12, a13, a14, a15, a16, a17, a19, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30, a31 }
         })
         return res.status(200).json(mes);
     } catch (error) {
